ee68927af455b0a2f809415387dd18378a8135f5
Stephen Maldonado <maldonado527@gmail.com>
2022-02-19 00:02:22 -0500

    Fixed version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='simpgenalg',
-    version='0.1.0',
+    version='0.1.1',
     description='Genetic Algorithm',
     url='https://github.com/StephenMal/simpgenalg',
     author='Stephen Maldonado',
